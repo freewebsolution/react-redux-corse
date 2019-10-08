ba2d5aae90360edbfdd877cbbf35d7ee4716605f
Lucio Ticali <info@freewebsolution.it>
2019-10-08 11:45:31 +0200

    Prpprietà e loro visualizzazione

--- esreact/src/App.js
+++ esreact/src/App.js
@@ -6,23 +6,26 @@ import Children from './components/Children';
 export class App extends Component {
   render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
         </a>
-        <Children></Children>
-      </header>
-    </div>
+            <Children nome='Mattia' cognome='Ticali'></Children>
+            <Children nome='Giorgio' cognome='Ticali' />
+          </header>
+        </div>
+      
     )
   }
 }
--- esreact/src/components/Children.js
+++ esreact/src/components/Children.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
  class Children extends Component {
+     constructor(props){
+         super(props);
+     }
     render() {
         return (
             <div>
-               <h1>Children</h1> 
+               <h1>Mio figlio {this.props.nome} {this.props.cognome}</h1> 
             </div>
         )
     }
 }
-
 export default Children
+
+// import React from 'react'
+// const Children =(props)=>{
+//     return(
+//         <div>
+//             <h1>Mio figlio {props.nome} {props.cognome}</h1>
+//         </div>
+//     )
+// }
+//  export default Children
+// props = {nome:'Mattia'}
+// this.props.nome
