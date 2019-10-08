4e3679ecae3b4bffa572d42eb70710f03e4f865e
Lucio Ticali <info@freewebsolution.it>
2019-10-08 10:42:41 +0200

    progettare un componente react

--- esreact/src/App.js
+++ esreact/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './css/App.css';
+import Children from './components/Children';
 
-function App() {
-  return (
+export class App extends Component {
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,9 +20,12 @@ function App() {
         >
           Learn React
         </a>
+        <Children></Children>
       </header>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
