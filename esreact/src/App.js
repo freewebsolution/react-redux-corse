32d7cdae16472016d5af5e2f5dfd2ea0d6013323
Lucio Ticali <info@freewebsolution.it>
2019-10-27 10:57:02 +0100

    Ciclo di vita dei componenti parte 2

--- esreact/src/App.js
+++ esreact/src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './css/App.css';
-import Children from './components/Children';
+import Stock from './components/stock/Stock';
+
 
 export class App extends Component {
+
   constructor(props) {
     super(props)
+    this.state = { listaStock: []}
     console.log(`1g) il costruttore crea la prima istanza Genitore`)
   }
 
   // -------MOUNTING CREAZIONE ----------
-  componentDidMount () {
-    console.log(`3g) Genitore didMount`)
+  componentDidMount() {
+    const stock = [
+
+      {
+        nome: 'APPL',
+        valore: 200
+      },
+      {
+        nome: 'GOOG',
+        valore: 350
+      }
+    ]
+    this.setState({listaStock: stock})
   }
 
   // --------UPDATE AGGIORNAMENTO--------
@@ -23,21 +37,35 @@ export class App extends Component {
   // componentDidUpdate() {
   //   console.log(`4g) DidUpdate padre ${this.state.nome}`)
   // }
+  aggiornoStock = (e) => {
+    e.preventDefault()
+    const stock1 = [
+
+      {
+        nome: 'AMZ',
+        valore: 250
+      },
+      {
+        nome: 'MICROSOFT',
+        valore: 750
+      },
+      {
+        nome: 'APPL',
+        valore: 1299
+      }
+    ]
+    this.setState({ listaStock:stock1 })
+  }
   render() {
     console.log(`2g) Genitore Render`)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-          <Children />
+          <p>
+            Applicazione Stock Exchange - <a href='/#' onClick={this.aggiornoStock} > Top guadagno Aggiorno</a>
+          </p>
+          {this.state.listaStock.map(el => <Stock dati = {el}/>)}
         </header>
       </div>
 
