e219506b245cc9d64dd0222b99ca7a9e29e336b7
Lucio Ticali <info@freewebsolution.it>
2019-11-15 10:42:52 +0100

    Chiamata End Point

--- book/src/components/login/login.js
+++ book/src/components/login/login.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import LoginService from './loginService'
 
 export class login extends Component {
     constructor(props) {
         super(props)
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            showSuccess:false,
+            showError:false,
+            errorMessage:'',
+            successMessage:''
         }
+        this.LoginService = new LoginService();
     }
     changeText = (e) => {
         let nam = e.target.name;
@@ -17,10 +23,58 @@ export class login extends Component {
         e.preventDefault();
     }
     login(e) {
+        this.LoginService.login(this.state.username,this.state.password,this.LoginSuccess,this.LoginError)
         console.log('Login con username: ', this.state.username)
         console.log('Login con password: ', this.state.password)
     }
+    LoginSuccess = (dataResult) =>{
+        this.setState({
+            showSuccess:true,
+            successMessage:`Login effettuato con successo il tuo token è: ${dataResult.token} `,
+            showError:false,
+            errorMessage:''
+        })
+        this.props.history.push('/booklist')
+    }
+    LoginError = (errorData) => {
+        this.setState({
+            showSuccess: false,
+            successMessage: '',
+            showError: true,
+            errorMessage: 'Login fallito per ' + errorData.error
+        })
+    }
+    getSuccessMessage =()=>{
+        if(this.state.showSuccess){
+            return (
+                <div style={{color:'green'}}>
+                    {this.state.successMessage}
+                </div>
+            );
+        }else {
+            return (
+                <div>
+                </div>
+            ); 
+        }
+    }
+    getErrorMessage = () => {
+        if (this.state.errorMessage) {
+            return (
+                <div style={{ color: 'red' }}>
+                    {this.state.errorMessage}
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                </div>
+            );
+        }
+    }
     render() {
+        var successMessage = this.getSuccessMessage();
+        var errorMessage = this.getErrorMessage();
         return (
             <div style={{ marginTop: "100px", minHeight: "70vh" }}>
                 <div className="container">
@@ -52,6 +106,8 @@ export class login extends Component {
                                 >
                                     Invio
 								</button>
+                                {successMessage}
+                                {errorMessage}
                             </form>
                         </div>
                     </div>
---
+++ book/src/components/login/loginService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default class LoginService {
+    login(email, password, onSuccess, onError){
+        axios.post("https://reqres.in/api/login", {email:email,password:password}).then(function(result){
+            console.log('Login effettuato con successo, token: ', result.data);
+            onSuccess(result.data);
+        },function(error){
+            console.error('Errore di login: ',error);
+            onError(error.response.data)
+        }
+        )
+    }
+}
