735f9a463cb053d0b7ffd2e693c6a13f1e601394
Lucio Ticali <info@freewebsolution.it>
2019-11-16 18:26:52 +0100

    Mostare i dati dei li bri a video

--- book/src/components/booklist/booklist.js
+++ book/src/components/booklist/booklist.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react'
 import '../../css/booklist/bookList.css'
 class booklist extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            searchTerm:''
+        this.state = {
+            searchTerm: 'Ceh',
+            books: { items: [] },
+            visible:{display:'block'}
         }
     }
-
-    fetchBooks =()=>{
+    fetchBooks = () => {
         const API = `https://www.googleapis.com/books/v1/volumes?q=${this.state.searchTerm}`;
         fetch(API)
-        .then(r => r.json())
-        .then(r => {
-            this.setState({books: r})
-            console.log(`Recupero dati ${JSON.stringify(this.state.books)}`)
-        })
-        .catch((error) =>{
-            console.log('Fetch faiiled', error)
-        })
+            .then(r => r.json())
+            .then(r => {
+                this.setState({ books: r })
+                console.log(`Recupero dati ${JSON.stringify(this.state.books)}`)
+            })
+            .catch((error) => {
+                console.log('Fetch faiiled', error)
+            })
     }
 
-    onSubmitHandler =(e)=>{
+    onSubmitHandler = (e) => {
         e.preventDefault();
         this.fetchBooks();
+        this.setState({
+            visible:{
+                display:'none'
+            }
+        })
 
     }
-    onInputChange=(e)=>{
-        this.setState({searchTerm:e.target.value})
-
+    onInputChange = (e) => {
+        this.setState({ searchTerm: e.target.value })
     }
     render() {
         return (
@@ -44,6 +49,7 @@ class booklist extends Component {
                                     placeholder="book title..."
                                     value={this.searchTerm}
                                     onChange={this.onInputChange}
+
                                 />
                                 <button style={{ padding: '8px 15px', borderRadius: '5px' }} className='btn btn-warning' type="submit">
                                     <i className="fas fa-search"></i>
@@ -54,27 +60,46 @@ class booklist extends Component {
                 </div>
                 <div className="main" style={{ marginTop: '10px' }}>
                     <div className="bookshelf" id='bookshelf'>
-                        <div className="search-icon" data-reactid=".0.1.0.0">
+                        <div style={this.state.visible} className="search-icon" data-reactid=".0.1.0.0">
                             <i className="fas fa-search"></i>
                         </div>
 
-                        <figure className='details-open'>
-                            <div className="perspective">
-                                <div className="book">
-                                    <div className="cover">
-                                        <div className="front">
+                        {
+                            this.state.books.items.map((book, index) => {
+                                const background =`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`;
+                                return (
+                                    <figure key={index} className='details-open'>
+                                        <div className="perspective">
+                                            <div className="book">
+                                                <div className="cover">
+                                                    <div className="front" style={{background: "url(" +background+") 0% 0% /100% 100% no-repeat",border: "2px solid rgb(238,238238)",width:'170px',height:'240px'}}>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="buttons">
-                            </div>
-                            <figcaption>
-                                <h2>
+                                        <div className="buttons">
+                                            <button style={{padding:'3px', borderRadius:'5px'}} className='btn btn-warning btn-sm'>Dettagli</button>
+                                            <a href={book.volumeInfo.infoLink}>Preview</a>
+                                        </div>
+                                        <figcaption>
+                                            <h2>
+                                                <span>
+                                                    {book.volumeInfo.title}
+                                                </span>
+                                                <span>
+                                                    {book.volumeInfo.authors}
+                                                </span>
+                                                <span>
+                                                    {book.volumeInfo.publishedDate}
+                                                </span>
+                                                
+                                            </h2>
+                                        </figcaption>
+                                    </figure>
 
-                                </h2>
-                            </figcaption>
-                        </figure>
+                                )
+                            })
+                        }
 
                     </div>
 
