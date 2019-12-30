0aaee0f87dd0366ca161f18b999a900771082fdf
lucio ticali <info@freewebsolution.it>
2019-12-30 15:45:23 +0100

    AGGIUNTA EROE METODO POST

--- reactHooks/heroes/src/App.js
+++ reactHooks/heroes/src/App.js
@@ -1,18 +1,18 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from './components/Hero';
 import axios from 'axios'
 const App = () => {
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log('effect')
     axios
-    .get('http://localhost:3001/heroes')
-    .then(response => {
-      console.log('promise fulfilled')
-      setHeroes(response.data)
-    })
-  } ,[])
-  const [heroes,setHeroes] = useState([])
+      .get('http://localhost:3001/heroes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setHeroes(response.data)
+      })
+  }, [])
+  const [heroes, setHeroes] = useState([])
   const [newHero, setNewHero] = useState('')
   const [showAll, setShowAll] = useState(true)
 
@@ -20,26 +20,31 @@ const App = () => {
   const heroesToShow = showAll ? heroes : heroes.filter(hero => hero.important === true)
   const rows = () => heroes.map(hero =>
     <Hero
-     key={hero.id}
-     hero={hero}
-     >
+      key={hero.id}
+      hero={hero}
+    >
     </Hero>)
 
-    const addHero = e => {
-      e.preventDefault();
-      const heroObject = {
-        name:newHero,
-        date: new Date().toISOString(),
-        important: Math.random() > 0.5,
-        id: heroes.lenght + 1 
-      }
-      setHeroes(heroes.concat(heroObject))
-      setNewHero('')
+  const addHero = e => {
+    e.preventDefault();
+    const heroObject = {
+      name: newHero,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: heroes.lenght + 1
     }
-    const handleHeroChange = e => {
-      console.log(e.target.value)
-      setNewHero(e.target.value)
-    }
+    axios
+      .post('http://localhost:3001/heroes', heroObject)
+      .then(response => {
+        setHeroes(heroes.concat(heroObject))
+        setNewHero('')
+      })
+
+  }
+  const handleHeroChange = e => {
+    console.log(e.target.value)
+    setNewHero(e.target.value)
+  }
   return (
 
     <div className="App">
@@ -50,14 +55,14 @@ const App = () => {
       <ul>
         {rows()}
       </ul>
-    <form onSubmit={addHero}>
-      <input 
-      type="text"
-      value={newHero}
-      onChange={handleHeroChange}
-      />
-      <button type='submit'>Add</button>
-    </form>
+      <form onSubmit={addHero}>
+        <input
+          type="text"
+          value={newHero}
+          onChange={handleHeroChange}
+        />
+        <button type='submit'>Add</button>
+      </form>
     </div>
   );
 }
