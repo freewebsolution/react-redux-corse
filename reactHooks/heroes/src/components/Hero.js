a8e3705a33e5cd9f29cb9a334f4c68a232f6e91e
Lucio Ticali <info@freewebsolution.it>
2020-01-03 10:53:46 +0100

    installazione todo-list con redux

--- reactHooks/heroes/src/components/Hero.js
+++ reactHooks/heroes/src/components/Hero.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Hero = ({ hero, toggleImportance,deleteHero }) => {
-    const label = hero.important ? 'Remove important' : 'Add important'
+    // const label = hero.important ? 'Remove important' : 'Add important'
     return (
-        <li>
-            {hero.name}
-            <button onClick={toggleImportance}>
-                {label}
-            </button>
-            <button style={{marginLeft:'10px'}} onClick={deleteHero}>Delete</button>
+        <li className='collection-item' style={{ borderBottom: '1px solid #dadada' }}>
+            <span className='title'>{hero.name}</span>
+            <span className="secondary-content">
+                <button className='waves-effect waves-light btn-small ' onClick={toggleImportance}>{hero.important ? 'not important' : <i className="material-icons">grade</i>}</button>
+                <button style={{ marginLeft: '5px' }} className='btn-floating btn-small waves-effect waves-light red' onClick={deleteHero}> <i className="material-icons">delete</i></button>
+            </span>
         </li>
     );
 }
