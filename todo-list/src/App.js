26ff766e5008278998be31b7f09585e271db5afc
lucio ticali <info@freewebsolution.it>
2020-01-03 17:18:33 +0100

    REMOVE_TODO CANCELLARE DALLA LISTA

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -9,9 +9,28 @@ let todos = [
   'andare in banca'
 ]
 function storeReducer(state = {}, action) {
-  console.log('Reduce', state)
-  return {...state};
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        todos: [
+          ...state.todos,
+          action.payload
+        ]
+      }
+    case 'REMOVE_TODO':
+      return {
+        todos: [
+          ...state.todos.slice(0, action.id),
+          ...state.todos.slice(action.id + 1)
+        ]
+      }
+    default:
+      return { ...state };
+  }
 }
+const store = createStore(storeReducer, { todos: [...todos] })
+console.log(store.getState())
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,13 +40,26 @@ class App extends Component {
     this.todoInput = React.createRef()
   }
   componentDidMount() {
-    const store = createStore(storeReducer, {todos:[...todos]})
-    console.log(store.getState())
-    this.setState({todos: [...store.getState().todos]})
+    this.setState({ todos: [...store.getState().todos] })
+    store.subscribe(() => {
+      this.setState({ todos: [...store.getState().todos] })
+      console.log(store.getState())
+    })
   }
   addTodo = () => {
     const todo = this.todoInput.current.value;
-    alert(todo)
+    store.dispatch({
+      type: 'ADD_TODO',
+      payload: todo
+    })
+  }
+
+  removeTodo = i => {
+    console.log(i)
+    store.dispatch({
+      type: 'REMOVE_TODO',
+      id: i
+    })
   }
 
   render() {
@@ -37,10 +69,10 @@ class App extends Component {
           <h1>Todo List</h1>
         </header>
         <button onClick={this.addTodo}>Add</button>
-        <input ref={this.todoInput}/>
+        <input ref={this.todoInput} />
         <ul>
           {
-            this.state.todos.map((todo, i) => <li key={i}>{todo}</li>)
+            this.state.todos.map((todo, i) => <li key={i}>{todo}<button onClick={this.removeTodo.bind(null, i)}> - </button></li>)
           }
         </ul>
       </div>
